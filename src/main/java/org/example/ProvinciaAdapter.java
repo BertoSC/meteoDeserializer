e7555638c1c4501ffaa0b74dc75f617f9543f83d
BertoSC <a23albertogc@iessanclemente.net>
2024-11-10 16:45:06 +0100

    ProvinciaAdapter done

--- src/main/java/org/example/ProvinciaAdapter.java
+++ src/main/java/org/example/ProvinciaAdapter.java
@@ -11,23 +11,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProvinciaAdapter implements JsonDeserializer<List<Provincia>> {
+    // creamos el tipo personalizado para poder invocar el adaptador de Lista de Concellos
     Type listType = new TypeToken<List<Concello>>(){}.getType();
 
     @Override
     public List<Provincia> deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+          // asociamos el objeto general a un JsonObject e iniciamos una lista de Provincia de concentra las provincias
           JsonObject jo= jsonElement.getAsJsonObject();
           List<Provincia> prov = new ArrayList<>();
+          // usamos un ENUM para identificar las cuatro provincias y, por cada constante, deserializamos la provincia correspondiente
           for (TipoProvincia tp:TipoProvincia.values()){
             TipoProvincia paraAñadirTipo = tp.getTipo(tp.getTipo());
+            // asociamos el objeto a un JsonArray e invocamos el deserializador de Lista de Concellos, al que le pasamos el JA para que pille los datos
             JsonArray ja=jo.getAsJsonArray(tp.getTipo());
-            String nom= "";
-            int id=0;
             List<Concello> listC = jsonDeserializationContext.deserialize(ja,listType);
+            // Asociamos las variables al obj Provincia y lo añadimos a la lista
             Provincia temp= new Provincia(paraAñadirTipo, tp.getTipo(), listC);
             prov.add(temp);
 
         }
-
 
         return prov;
         }
